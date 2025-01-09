b5912814042cbf670ab9ff801a5a9e15d9042894
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-09 13:18:57 +0100

    MainNav

---
+++ src/components/MainNav.jsx
@@ -0,0 +1,31 @@
+function MainNav() {
+    const menu = [
+        {
+            path: "/",
+            title: "Home"
+        },
+        {
+            path: "/aboutus",
+            title: "About"
+        },
+        {
+            path: "/posts",
+            title: "Posts"
+        }
+    ];
+
+    return (
+
+        <nav>
+            <ul>
+                {menu.map((curItem) => (
+                    <li>
+                        <NavLink to={curItem.path}>{curItem.title}</NavLink>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}
+
+export default MainNav;
