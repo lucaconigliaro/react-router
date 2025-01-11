493a75415535690c7e971fbcb12c1b58148678d1
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-11 14:38:15 +0100

    Create post and Posts

--- src/pages/Posts/CreatePost.jsx
+++ src/pages/Posts/CreatePost.jsx
@@ -1,7 +1,94 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const initialFormData = {
+    id: "",
+    titolo: "",
+    immagine: "",
+    contenuto: "",
+};
+
+
+const handleArticleForm = (event) => {
+    event.preventDefault();
+    axios.post(`${apiUrl}/posts`, formData)
+        .then((resp) => {
+            const newArray = [...article, resp.data];
+            setArticle(newArray);
+            setFormData(initialFormData);
+        })
+};
+
+const handleInputChange = (event) => {
+    const keyToChange = event.target.name;
+    const newValue = event.target.value;
+
+    const newData = {
+        ...formData,
+        [keyToChange]: newValue
+    };
+
+    setFormData(newData);
+};
+
+
 function CreatePost() {
-    return(
-        <>
-        </>
+    return (
+        <div className="container">
+            <section>
+                <h3>Aggiungi un nuovo articolo</h3>
+                <form onSubmit={handleArticleForm}>
+                    <div>
+                        <label htmlFor="articleTitle">Nome dell'articolo</label>
+                        <input
+                            id="articleTitle"
+                            type="text"
+                            className="form-control mb-2"
+                            placeholder="Inserisci il titolo dell'articolo"
+                            name="titolo"
+                            value={formData.titolo}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="articleImage">Immagine dell'articolo</label>
+                        <input
+                            id="articleImage"
+                            type="text"
+                            className="form-control mb-2"
+                            placeholder="Inserisci l'immagine dell'articolo"
+                            name="immagine"
+                            value={formData.immagine}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="articleContent">Contenuto dell'articolo</label>
+                        <textarea
+                            id="articleContent"
+                            className="form-control mb-2"
+                            placeholder="Inserisci il contenuto dell'articolo"
+                            name="contenuto"
+                            value={formData.contenuto}
+                            onChange={handleInputChange}
+                        ></textarea>
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="btn btn-success mt-2"
+                        disabled={!formData.titolo || !formData.contenuto}
+                    >
+                        Invia
+                    </button>
+
+                </form>
+            </section>
+        </div>
     )
 }
 
--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -1,8 +1,50 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AppCard from "./AppCard";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+
 function Posts() {
-    return(
-        <>
-        </>
-    )
+    const [article, setArticle] = useState([]);
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+
+    const getPosts = () => {
+        axios.get(`${apiUrl}/posts`)
+            .then((resp) => {
+                setArticle(resp.data.data);
+            })
+    };
+
+    const cancel = (idToDelete) => {
+        axios.delete(`${apiUrl}/posts/${idToDelete}`)
+            .then(() => {
+                const newArray = article.filter((curArticle) => curArticle.id !== idToDelete);
+                setArticle(newArray);
+            })
+    };
+
+    return (
+        <div className="container">
+            <section>
+                <h2>Nuovi Articoli</h2>
+                {article.length > 0 ? (
+                    <div className="card-container">
+                        {article.map((curArticle) => (
+                            <div className="col" key={curArticle.id}>
+                                <AppCard post={curArticle} onCancel={() => cancel(curArticle.id)} />
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <p>Nessun articolo</p>
+                )}
+            </section>
+        </div>
+    );
 }
 
 export default Posts;
